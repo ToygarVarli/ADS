dcc10a1db3c7c6475a23eeb6c1e21e08d98868ed
Mustafa Toygar Varli <tvarli@ahit.com.tr>
2024-09-18 16:09:06 +0300

    s:t

--- AdsLib/AdsWrapper.cpp
+++ AdsLib/AdsWrapper.cpp
@@ -11,6 +11,8 @@ AdsWrapper::~AdsWrapper() {
     // Destructor implementation
 }
 
+
+
 void AdsWrapper::writeValue(AmsNetId remoteNetId, char remoteIpV4[], int16_t value, const std::string& symbol) {
     try {
         //static const AmsNetId remoteNetId { 169,254,53,70,1,1 };
@@ -26,8 +28,34 @@ void AdsWrapper::writeValue(AmsNetId remoteNetId, char remoteIpV4[], int16_t val
     }
 }
 
+extern "C" {
+void writeValueTestWrapper()
+{
+    static AmsNetId remoteNetId { 169,254,53,70,1,1 };
+    static char remoteIpV4[] = "192.168.110.107"; 
 
-void AdsWrapper::writeValueTest(){
+    // Remote PLC bilgileri
+     int16_t value = 12345;                        
+     std::string symbol = "ToygarMAIN.toygar";  
+
+     try {
+        //static const AmsNetId remoteNetId { 169,254,53,70,1,1 };
+         //static const char remoteIpV4[] = "192.168.110.107";
+        AdsDevice route {remoteIpV4, remoteNetId, AMSPORT_R0_PLC_TC3};
+        AdsVariable<int16_t> simpleVar {route, symbol};
+
+        simpleVar = value;  // Değeri yaz
+
+        std::cout << "Successfully wrote " << value << " to symbol: " << symbol << std::endl;
+    } catch (const std::exception& ex) {
+        std::cerr << "Error writing value: " << ex.what() << std::endl;
+    }
+}
+}
+
+
+
+/*void AdsWrapper::writeValueTest(){
     static AmsNetId remoteNetId { 169,254,53,70,1,1 };
     static char remoteIpV4[] = "192.168.110.107"; 
     AdsWrapper adsWrapper;
@@ -48,4 +76,4 @@ void AdsWrapper::writeValueTest(){
     } catch (const std::exception& ex) {
         std::cerr << "Error writing value: " << ex.what() << std::endl;
     }
-}
+}*/
--- AdsLib/AdsWrapper.h
+++ AdsLib/AdsWrapper.h
@@ -1,16 +1,42 @@
+#ifdef BUILDING_DLL
+#define ADSWRAPPER_API __declspec(dllexport)
+#else 
+#define ADSWRAPPER_API __declspec(dllimport)
+#endif
+
+
 #ifndef ADSWRAPPER_H
 #define ADSWRAPPER_H
 
 #include "AdsLib.h"
 #include <string>
 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+// writeValue fonksiyonu için basitleştirilmiş versiyon
+
+
+// writeValueTest fonksiyonu için basitleştirilmiş versiyon
+ADSWRAPPER_API void writeValueTest();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
 class AdsWrapper {
 public:
     AdsWrapper();
     ~AdsWrapper();
 
-    void writeValue(AmsNetId remoteNetId, char remoteIpV4[], int16_t value, const std::string& symbol);
-    void writeValueTest();
+     void writeValue(AmsNetId remoteNetId, char remoteIpV4[], int16_t value, const std::string& symbol);
+     //void writeValueTest();
 };
 
 #endif // ADSWRAPPER_H
